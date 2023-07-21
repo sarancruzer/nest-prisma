e27767866e788ad04645cb6995ac6c238f27bb9d
Saravanan Nandhan <sarancruzer@gmail.com>
2023-07-21 12:39:32 +0530

    Logger added in main ts file

--- src/main.ts
+++ src/main.ts
@@ -43,6 +43,9 @@ async function bootstrap() {
   Logger.log(`Application is running on: ${await app.getUrl()}`);
   Logger.log(`Application is running in : ${await config.get('ENVIRONMENT')} mode`);
 
+  Logger.log(`Application is running on: ${await app.getUrl()}`);
+  Logger.log(`Application is running in : ${await config.get('ENVIRONMENT')} mode`);
+
   
 }
 bootstrap();
